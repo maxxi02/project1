c9740b211860b596badbcf64d9833b9c4ea1d2a5
maxxi <129664117+maxxi02@users.noreply.github.com>
2023-12-18 22:09:09 +0800

    pagmamahal ni rojan#2

--- app/forecastpage/page.tsx
+++ app/forecastpage/page.tsx
@@ -8,7 +8,7 @@ function ForecastPage() {
   return (
     <div className="min-h-screen p-10 flex flex-col items-center justify-center">
       <Card className="max-w-4xl mx-auto text-center bg-gradient-to-r from-cyan-500 to-blue-500">
-        <Heading as={'h1'} size={'4xl'}>Choose a city!</Heading>
+        <Heading as={'h1'} size={'4xl'} className="text-[whitesmoke]">Choose a city!</Heading>
         <Divider className="my-10"/>
         <Card className="p-0">
           {/* city picker */}
--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -17,94 +17,97 @@ type Props =
 
 async function page( {params:{city, long, lat}}: Props) {
     const decodeURI = decodeURIComponent(city)
-    const client = getClient()
-    const {data} = await client.query(
-      {
-        query: fetchWeatherQuery,
-        variables:
-        {
-          current_weather: "true",
-          longitude: long,
-          latitude: lat,
-          timezone: "auto",
-          temperature_unit: 'fahrenheit',
-        }
-      }
-    )
-
-    const results: Root = data.myQuery;
-    console.log(results);
-
-    return (
-      <div>
-        <InformationPanel 
-        results={results} 
-        city={city} 
-        lat={lat} 
-        long={long}/>
-          <div className="flex flex-col w-full px-5">
-
-            <div className="p-5">
-              <div className="pb-5">
-                <Heading className="py-2">Current weather</Heading>
-                <p className="text-sm text-gray-400">
-                  Last Updated at:{" "}
-                  {new Date(results.current_weather.time).toLocaleString()} ({results.timezone})
-                </p>
-              </div>
-        
-                <div className="m-2 mb-10">
-                  {/* calloutcard */}
-                  <CallOutCard message="Explore live city weather updates and learn through interactive graphs." />
-                </div>
-        
-              <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
-                {/* statcard */}
-                <StatCard
-                  title="Maximum Temperature"
-                  metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
-                  color="yellow"
-                  />
-                <StatCard
-                  title="Minimum Temperature"
-                  metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
-                  color="green"
-                />
-                <div className="flex flex-col items-center w-full gap-3">
-                  <StatCard
-                    title="UV Index"
-                    metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
-                    color="rose"
+    try {
+        const client = getClient()
+        const {data} = await client.query(
+          {
+            query: fetchWeatherQuery,
+            variables:
+            {
+              current_weather: "true",
+              longitude: long,
+              latitude: lat,
+              timezone: "auto",
+              temperature_unit: 'fahrenheit',
+            }
+          }
+        )
+        const results: Root = data.myQuery;
+        console.log(results);
+        return (
+          <div>
+            <InformationPanel 
+            results={results} 
+            city={city} 
+            lat={lat} 
+            long={long}/>
+              <div className="flex flex-col w-full px-5">
+    
+                <div className="p-5">
+                  <div className="pb-5">
+                    <Heading className="py-2">Current weather</Heading>
+                    <p className="text-sm text-gray-400">
+                      Last Updated at:{" "}
+                      {new Date(results.current_weather.time).toLocaleString()} ({results.timezone})
+                    </p>
+                  </div>
+            
+                    <div className="m-2 mb-10">
+                      {/* calloutcard */}
+                      <CallOutCard message="Explore live city weather updates and learn through interactive graphs." />
+                    </div>
+            
+                  <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
+                    {/* statcard */}
+                    <StatCard
+                      title="Maximum Temperature"
+                      metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
+                      color="yellow"
+                      />
+                    <StatCard
+                      title="Minimum Temperature"
+                      metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
+                      color="green"
                     />
-                  {Number(results.daily.uv_index_max[0].toFixed(1)) > 7 && (
-                    <CallOutCard
-                      warning
-                      message={`UV index in ${decodeURI} is high today, make sure to wear Sun Protection Factor (SPF)`}
-                      />)}
-                </div>
-
-                <div className="flex space-x-3">
-                  <StatCard
-                      title="Wind Speed"
-                      metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
-                      color="cyan"
-                      />
-                  <StatCard
-                      title="Wind Direction"
-                      metric={`${results.current_weather.winddirection.toFixed(1)}°`}
-                      color="violet"
-                      />
-                </div>
+                    <div className="flex flex-col items-center w-full gap-3">
+                      <StatCard
+                        title="UV Index"
+                        metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
+                        color="rose"
+                        />
+                      {Number(results.daily.uv_index_max[0].toFixed(1)) > 7 && (
+                        <CallOutCard
+                          warning
+                          message={`UV index in ${decodeURI} is high today, make sure to wear Sun Protection Factor (SPF)`}
+                          />)}
+                    </div>
+    
+                    <div className="flex space-x-3">
+                      <StatCard
+                          title="Wind Speed"
+                          metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+                          color="cyan"
+                          />
+                      <StatCard
+                          title="Wind Direction"
+                          metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+                          color="violet"
+                          />
+                    </div>
+                  </div>
               </div>
+              <Divider/>
+              {/* charts */}
+              <div className="space-y-3">
+    
+              </div>
+            </div>
           </div>
-          <Divider/>
-          {/* charts */}
-          <div className="space-y-3">
-
-          </div>
-        </div>
-      </div>
-    )};
+        )
+      
+    } catch (error) {
+      console.log("Something went wrong!");
+  }};
     
 
 export default page;
