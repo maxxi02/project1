b5a495cf15603fb01b4f40195c14ed13c219366e
maxxi <129664117+maxxi02@users.noreply.github.com>
2023-12-18 22:36:36 +0800

    hhh

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,49 +1,39 @@
-
-import { Button, Card, Heading, Highlight, Text } from '@chakra-ui/react'
+"use client";
+import { Card, Highlight } from '@chakra-ui/react'
+import { Button } from '@tremor/react';
 import Link from 'next/link'
 
 function HomePage() {
-
-
   return (
     <div className='flex items-center h-screen w-full max-w-[1600px] mx-auto gap-[100px]'>
       <div className='flex-1 flex flex-col items-start'>
-        <Heading lineHeight='normal'>
-          <Heading className='py-2' as='h4' size='md'>
             Welcome to WeatherEdu
-          </Heading>
-          <Text  fontSize={'6xl'}>Exploring The{" "}  
-          <Highlight query={''} styles={{bg:'orange.100', rounded: 'md', py: '1', px: '2'}}>
-            Fascinating World Of Weather Education
-          </Highlight>
-          </Text>
-        </Heading>
-        <div className='flex flex-col item-start gap-3'>
-          <Text fontSize='md' className=''>
-            <Highlight query={'Powered by live Forecast Function'} styles={{bg:'orange.100', rounded: 'md', py: '1', px: '2'}}>
-            Powered by live Forecast Function
+          <h2 className='text-4xl leading-10 py-4 my-2'>
+            Exploring The{' '}
+            <Highlight query={''} styles={{ bg: 'orange.100', rounded: 'md', py: '1', px: '2' }}>
+              Fascinating World Of Weather Education
             </Highlight>
-          </Text>
-          <Link href={'/forecastpage'} className='py-2'>
-          <Button colorScheme='black.500' variant='outline'>
-            Try it now!
-          </Button>
+          </h2>
+        <div className='flex flex-col item-start gap-3'>
+          <h2>
+            <Highlight query={'Powered by live Forecast Function'} styles={{ bg: 'orange.100', rounded: 'md', py: '1', px: '2' }}>
+              Powered by live Forecast Function
+            </Highlight>
+          </h2>
+          <Link href='/forecastpage' passHref>
+            <Button >
+              Try it now!
+            </Button>
           </Link>
         </div>
       </div>
       <div className='flex-1'>
-        <Card className='w-full p-3' colorScheme={'red'} variant={'unstyled'}>
-          <video
-          className='w-[100%] h-[100%] bg-cover rounded-md' 
-          src="/bg.mp4" 
-          autoPlay
-          muted
-          loop
-          />
+        <Card className='w-full p-3' variant='unstyled'>
+          <video className='w-full h-full bg-cover rounded-md' src='/bg.mp4' autoPlay muted loop />
         </Card>
       </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
